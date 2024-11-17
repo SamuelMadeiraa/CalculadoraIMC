669dcd2b24a690ce0c0e2b6e812b1c4f5f19db79
Samuel Madeira <samuelmadeiradev@gmail.com>
2024-11-17 11:08:24 -0300

    Adiciona função classificar_imc para categorizar o IMC em faixas de peso

--- CalculadoraIMc/CalculadoraImc.py
+++ CalculadoraIMc/CalculadoraImc.py
@@ -1,3 +1,17 @@
 def calcular_imc(peso, altura):
     """Calcula o IMC baseado no peso e altura."""
     return peso / (altura ** 2)
+def classificar_imc(imc):
+    """Classifica o IMC em categorias."""
+    if imc < 18.05:
+        return 'Abaixo do peso ideal'
+    elif 18.05 <= imc <= 24.9:
+        return 'Peso normal'
+    elif 25 <= imc < 29.9:
+        return 'Sobrepeso'
+    elif 30 <= imc < 34.9:
+        return 'Obesidade grau I'
+    elif 35 <= imc < 39.9:
+        return 'Obesidade grau II'
+    else:
+        return 'Obesidade mórbida'
